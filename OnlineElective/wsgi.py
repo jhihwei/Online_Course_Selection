d470549ff6618ceef31588ff6105c3e97efe979c
Jhih-Wei Jhan <krel.jhan@gmail.com>
2020-10-05 14:04:09 +0800

    .

--- OnlineElective/wsgi.py
+++ OnlineElective/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from dj_static import Cling # <- 加入
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OnlineElective.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+application = Cling(get_wsgi_application())
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --pythonpath mysite OnlineElective.wsgi --log-file -
