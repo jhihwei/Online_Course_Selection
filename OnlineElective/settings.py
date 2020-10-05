6dfe98ece25659380eccf5f25e309953ae490632
Jhih-Wei Jhan <krel.jhan@gmail.com>
2020-10-05 14:49:01 +0800

    .

--- OnlineElective/settings.py
+++ OnlineElective/settings.py
@@ -131,8 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '~/OnlineElective/app/static/',
-]
+STATIC_URL = '~/OnlineElective/app/static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     '~/OnlineElective/app/static/',
+# ]
