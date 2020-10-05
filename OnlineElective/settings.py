12dad291fc6b322b3d8a1896eaadebea3be85968
Jhih-Wei Jhan <krel.jhan@gmail.com>
2020-10-05 14:56:16 +0800

    .

--- OnlineElective/settings.py
+++ OnlineElective/settings.py
@@ -133,6 +133,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-#     '~/OnlineElective/app/static/',
+#     os.path.join(BASE_DIR, "static"),
+#     '/var/www/static/',
 # ]
