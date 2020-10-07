f088cf449ff45b1726a7600af4ed6198747ae53d
Jhih-Wei Jhan <krel.jhan@gmail.com>
2020-10-07 09:33:50 +0800

    .

--- app/admin.py
+++ app/admin.py
@@ -7,18 +7,18 @@ admin.AdminSite.site_header = '線上選課系統'
 
 
 @admin.register(Course)
-class Course(ImportExportModelAdmin):
+class Course_admin(ImportExportModelAdmin):
     list_display = ('name_code', 'name', 'description')
 
 
 @admin.register(Students)
-class Students(ImportExportModelAdmin):
+class Students_admin(ImportExportModelAdmin):
     list_display = ('class_code', 'number', 'seat_number', 'name', 'gender')
     list_display_links = ('name',)
 
 
 @admin.register(Group_record)
-class Group_record(ImportExportModelAdmin):
+class Group_record_admin(ImportExportModelAdmin):
 
     def get_description(self, obj):
         return obj.course.description
@@ -33,10 +33,15 @@ class Group_record(ImportExportModelAdmin):
     readonly_fields=('student', 'timestamp')
 
 @admin.register(Course_record)
-class Course_record(ImportExportModelAdmin):
+class Course_record_admin(ImportExportModelAdmin):
     def get_description(self, obj):
         return obj.course.description
     get_description.short_description = '描述'
 
-    list_display = ('course', 'get_description', 'student', 'course_order', 'allocation', 'timestamp')
+    def get_class_code(self, obj):
+        return obj.student.class_code
+    get_class_code.short_description = '班級'
+
+    list_display = ('course', 'get_description', 'get_class_code', 'student', 'course_order', 'allocation', 'timestamp')
     list_display_links = ('student',)
+    search_fields = ['student__class_code']
